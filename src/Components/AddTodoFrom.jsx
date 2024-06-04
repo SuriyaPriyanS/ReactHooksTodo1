21dc256da3df2c938b275f3dec0415e26cdeab20
Suriya Priyan <suriyapriyan0506@gmail.com>
2024-06-04 16:04:16 +0530

    upadted

--- src/Components/AddTodoFrom.jsx
+++ src/Components/AddTodoFrom.jsx
@@ -1,40 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const AddTodoFrom = ({onAdd}) => {
+const AddTodoForm = ({ onAdd }) => {
+  const [name, setName] = useState("");
+  const [descripation, setDescripation] = useState("");
+  const [error, setError] = useState("");
 
-    const [name , setName]= useState("");
-    const [descripation, setDescripation] = useState("");
-    const [error, setError] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    const handleSubmit = (e)=>{
-        e.preventDefault();
+    if (!name || !descripation) {
+      setError("Please fill in all fields");
+      return;
+    }
 
-        if(name || descripation){
-            setError("please fill in fields");
-            return;
-        }
+    onAdd({ name, descripation });
+    setName("");
+    setDescripation("");
+    setError("");
+  };
 
-        onAdd({name, descripation});
-        setName("");
-        setDescripation("");
-        setError("");
-    };
-    return (
-        <div className='add- todo'>
-            
-            <from onSubmit ={handleSubmit}>
-                <div>
-                    <input type="text" placeholder='Todo Name' value={name} onChange={(e)=>{
-                        setName(e.target.value)}}/>
-                        
-                        <input  placeholder='description' value={descripation} onChange={(e)=>{
-                        setDescripation(e.target.value)}}/>
-                        <buton type ="submit">Add Todo</buton>
-                </div>
-                {error && <p className='error-message danget'>{error}</p>}
-            </from>
+  return (
+    <div className="add-todo">
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="text"
+            placeholder="Todo Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            placeholder="Description"
+            value={descripation}
+            onChange={(e) => setDescripation(e.target.value)}
+          />
+          <button type="submit">Add Todo</button>
         </div>
-    );
+        {error && <p className="error-message danger">{error}</p>}
+      </form>
+    </div>
+  );
 };
 
-export default AddTodoFrom;
+export default AddTodoForm;
